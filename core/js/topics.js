c53bf4415d504bacadefee9aa3983dca1738ac2b
JH-BKI <jharrison@kangan.edu.au>
2025-09-09 14:18:28 +1000

    Update topics.js

--- core/js/topics.js
+++ core/js/topics.js
@@ -288,8 +288,8 @@ const topicData = {
                 { 
                   id: 's01-background', 
                   src: './assets/topic_1/background.png',
-                  scale: '2.5 1.1 0.5',
-                  position: '0 0.25 -2',
+                  scale: '4.5 1.2 1',
+                  position: '0 0.5 -4',
                   rotation: '0 0 0',
                   visible: 'false',
                   opacity: '1',
